eb1a363671ed87a285c7bc2b7ed5d671c49d03eb
wwlee94 <wwlee94@naver.com>
2020-03-04 22:55:00 +0900

    create-react-app을 사용해 react app 생성

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Movie from './Movie';
+
+const movies = [
+  {
+    title: "React",
+    poster: "https://blog.itpaper.co.kr/attach/40-frontend-react/logo.jpg"
+  },
+  {
+    title: "Amazon Web Service",
+    poster: "https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F99D6533B5C5720CF09"
+  },
+  {
+    title: "Docker",
+    poster: "https://docs.docker.com/images/docs@2x.png"
+  },
+  {
+    title: "Express.js",
+    poster: "https://t1.daumcdn.net/cfile/tistory/264CA7335967150930"
+  }
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
-  );
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// 리액트돔은 1개의 컴포넌트를 렌더링(출력) 해줌 -> 어디에? 'root'에 !
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
